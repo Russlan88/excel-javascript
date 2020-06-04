cb31eb011059a533f94bae26d2c4bc4c045d6569
Ruslan <ruslan.ogarkov88@gmail.com>
2020-06-04 18:26:38 +0300

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "semi": "off",
+        "arrow-parents": "off"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+    }
+};
